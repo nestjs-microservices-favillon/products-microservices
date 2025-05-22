133d53dbaff4deb77d2dc7094f2e51f876fdbfe9
favillon <favillon@hotmail.com>
2025-05-22 16:16:13 -0500

    Modificando de REST a TCP para la comunicacion

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
   const logger = new Logger('Main');
-  
-  const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport : Transport.TCP,
+    options : {
+      port : envs.port,
+    }
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     })
   );
-  await app.listen(envs.port);
+  await app.listen();
+  // await app.startAllMicroservices(); //  Compatibilidad en REST Y TCP // si no instalacion solo de  @nestjs/microservices
+
   logger.log(`Products Microservice running on port ${ envs.port }`);
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,36 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
+import { Controller, ParseIntPipe } from '@nestjs/common';
 
 import { CreateProductDto } from './dto/create-product.dto';
 import { PaginationDto } from 'src/common/dto/paginationdto';
 import { ProductsService } from './products.service';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
+  //@Post()
+  @MessagePattern({ cmd : 'create_product'})
+  create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  @Get()
-  findAll( @Query() paginationDto: PaginationDto) {
+  //@Get()
+  @MessagePattern({ cmd : 'find_all_products'})
+  findAll( @Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
+  //@Get(':id')
+  @MessagePattern({ cmd : 'find_one_product'})
+  findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(id, updateProductDto);
+  //@Patch(':id')
+  @MessagePattern({ cmd : 'update_product'})
+  update(
+    //@Payload('id', ParseIntPipe) id: number,
+    //@Body() updateProductDto: UpdateProductDto
+    @Payload() updateProductDto: UpdateProductDto
+  ) {
+    return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
+  //@Delete(':id')
+  @MessagePattern({ cmd : 'remove_product'})
+  remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
 }
